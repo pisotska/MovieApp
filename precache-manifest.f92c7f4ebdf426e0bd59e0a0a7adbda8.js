5d9732cdfcdc8c437e72bfaec413869c24c3b03c
pisotska <mariya.777481@gmail.com>
2023-05-12 12:11:53 +0300

    Updates

---
+++ precache-manifest.f92c7f4ebdf426e0bd59e0a0a7adbda8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c779c66ddaad47ac11c8",
+    "url": "/MovieApp/static/css/main.a4cfb5dd.chunk.css"
+  },
+  {
+    "revision": "c779c66ddaad47ac11c8",
+    "url": "/MovieApp/static/js/main.c779c66d.chunk.js"
+  },
+  {
+    "revision": "f743b80cb398438e8cc5",
+    "url": "/MovieApp/static/js/1.f743b80c.chunk.js"
+  },
+  {
+    "revision": "5dfb1f54ad9e9ba2849e",
+    "url": "/MovieApp/static/js/runtime~main.5dfb1f54.js"
+  },
+  {
+    "revision": "0c85c49e6263218faf45e4057bc49c4c",
+    "url": "/MovieApp/index.html"
+  }
+];
